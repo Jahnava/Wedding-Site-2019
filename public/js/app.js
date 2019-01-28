5cd6867e0ff35b81eb6a387102b0b352680be49d
jahnava <jahnavalmt@gmail.com>
2019-01-28 13:39:43 -0500

    added redirect in app.js for mobile to go to home page instead of index.js

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+if (screen.width <= 699) {
+document.location = "/html/home.html";
+}
+
+if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+   location.replace("/html/home.html");
+}
