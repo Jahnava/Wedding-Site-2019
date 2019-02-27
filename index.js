f4593f503adf0a12a0571246e0aecccc3ca1def4
jahnava <jahnavalmt@gmail.com>
2019-02-26 21:17:52 -0500

    added pics

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ server.get('/', function(req, res){
   res.sendFile('index.html', {root: __dirname + '/public/html'});
 });
 
-// server.get('/index1', function(req, res){
-//   res.sendFile('index1.html', {root: __dirname + '/public/html'});
-// });
-//
-// server.get('/details', function(req, res){
-//   res.sendFile('details.html', {root: __dirname + '/public/html'});
-// });
-
-
-
 server.listen(port, function(){
   console.log('Now listening on port...', port);
 });
